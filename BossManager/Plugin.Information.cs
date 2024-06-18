3bda149343853c315c2d9e1f034455aa48d89544
Soof4 <emr3.cbc@gmail.com>
2024-06-18 15:21:24 +0300

    Remove unnecessary directories

--- BossManager/Plugin.Information.cs
+++ BossManager/Plugin.Information.cs
@@ -6,7 +6,7 @@ namespace BossManager
     public partial class Plugin: TerrariaPlugin
     {
         public override string Name => "BossManager";
-        public override Version Version => new(1, 3, 1);
+        public override Version Version => new(1, 3, 2);
         public override string Author => "Ozz5581";
         public override string Description => "Controls boss spawning requirements.";
 
